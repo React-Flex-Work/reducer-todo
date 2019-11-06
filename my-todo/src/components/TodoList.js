a6c9f4dffcd3a095c72d6f99ef0c64b106ac9e97
Eric Richards <esholundr@yahoo.com>
2019-11-06 15:43:17 -0700

    Added styling, mvp complete

--- my-todo/src/components/TodoList.js
+++ my-todo/src/components/TodoList.js
@@ -3,7 +3,7 @@ import Todo from './Todo';
 
 const TodoList = props => {
     return (
-        <div>
+        <div className="todo-text">
             {props.initialTodos.map(item => (
                 <Todo key={item.id} item={item} toggleCompleted={props.toggleCompleted} />
             ))}
